7f0abf9af80cb1afa21eedb973821bb3d8b56cfb
trunghieuvn <trunghieuvn@users.noreply.github.com>
2025-09-06 06:51:12 +0000

    deploy: 3c0e836ffc49e418e2caa724946cc3928adfb89b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"flutter_bootstrap.js": "4b9668648318c0effaa1ff9b49786576",
+"flutter_bootstrap.js": "12ecbf522419d699f7b0d73d4e3a1974",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "014d5850de4825ac80ed5a741a5fd613",
 "/": "014d5850de4825ac80ed5a741a5fd613"};
